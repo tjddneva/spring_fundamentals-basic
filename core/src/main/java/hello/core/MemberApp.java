092bd0a308f3f53c0d3d4d346679a15fbd230ebf
Sungwoo Park <tjddneva@naver.com>
2022-02-14 19:43:42 +0900

    not spring

---
+++ core/src/main/java/hello/core/MemberApp.java
@@ -0,0 +1,20 @@
+package hello.core;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+
+public class MemberApp {
+
+    public static void main(String[] args) {  //psvm 치고 엔터 누르면 메인 메소드 생성
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "memberA", Grade.VIP);  //ctrl alt v 변수 추출
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("new member = " + member.getName());
+        System.out.println("find Member = " + findMember.getName()); //soutv 위의 변수 값 출력 자동입력
+    }
+
+}
---
+++ core/src/main/java/hello/core/discount/DiscountPolicy.java
@@ -0,0 +1,9 @@
+package hello.core.discount;
+
+import hello.core.member.Member;
+
+public interface DiscountPolicy {
+
+    int discount(Member member, int price); //f2 누르면 오류 난 곳으로 이동
+
+}
